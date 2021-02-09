0ac6ee5626b77b815c65f68db5d9cd3e5b3cd760
Miyuki Sano <miyuki.wtnb@gmail.com>
2021-02-09 22:33:02 +0900

    Modify chart style to allow scroll x direction

--- ui/components/HistoryChart/ChartContainer/index.tsx
+++ ui/components/HistoryChart/ChartContainer/index.tsx
@@ -1,9 +1,15 @@
-import { FC } from 'react';
+import { FC, useEffect, useRef } from 'react';
 
 export const ChartContainer: FC = ({ children }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    containerRef.current.scrollLeft = 800;
+  }, []);
+
   return (
-    <div className="chart-container">
-      {children}
+    <div className="chart-container" ref={containerRef}>
+      <div className="chart-inner">{children}</div>
       <style jsx>{`
         .chart-container {
           height: 320px;
@@ -11,6 +17,13 @@ export const ChartContainer: FC = ({ children }) => {
           margin: 16px 0;
           background-color: #fff;
           border-radius: 8px;
+          overflow-x: scroll;
+          overflow-y: hidden;
+        }
+
+        .chart-inner {
+          width: 768px;
+          height: 320px;
         }
       `}</style>
     </div>
